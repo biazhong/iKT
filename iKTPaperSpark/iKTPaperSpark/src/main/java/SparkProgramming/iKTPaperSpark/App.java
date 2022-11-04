5e5f7dc9c6b457a2c1b45b2c76e6d35d16edf952
biazhong <45530078+biazhong@users.noreply.github.com>
2022-11-04 13:38:17 +0800

    Add files via upload

---
+++ iKTPaperSpark/iKTPaperSpark/src/main/java/SparkProgramming/iKTPaperSpark/App.java
@@ -0,0 +1,13 @@
+package SparkProgramming.iKTPaperSpark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
